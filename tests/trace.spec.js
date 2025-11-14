676cfedcc74390acc4cc856cdd319cdf4ccfa20c
madhavadi <madhav221095@gmail.com>
2025-11-14 11:27:59 +0530

    Updated again

--- tests/trace.spec.js
+++ tests/trace.spec.js
@@ -3,6 +3,6 @@ import {test,expect} from '@playwright/test';
 test('Wrong Intentional test',async ({page}) => {
     await page.goto('https://www.saucedemo.com');
 
-    await expect(page).toHaveTitle('Wrong title');
+    await expect(page).toHaveTitle('Swag Labs');
 }
 );
