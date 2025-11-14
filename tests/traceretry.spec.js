e3a3cdec2c65729abee55f7a884f40930c6e5557
madhavadi <madhav221095@gmail.com>
2025-11-14 12:31:39 +0530

    Updated

--- tests/traceretry.spec.js
+++
@@ -1,6 +0,0 @@
-import {test,expect} from '@playwright/test';
-
-test ('Retry test',async ({page})=> {
-    await page.goto('https://www.amazon.in');
-    await (page).getByText('Bestsellers').click();
-});
