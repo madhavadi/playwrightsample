08b04c5ad3f01ddbfaec4808921c2097e0cdf3b8
madhavadi <madhav221095@gmail.com>
2025-11-20 15:40:34 +0530

    Login updated

--- tests/logincheck.spec.js
+++ tests/logincheck.spec.js
@@ -1,8 +1,8 @@
-const { test,expect } = require('@playwright/test');
+import { test,expect } from '@playwright/test';
 
 test('Instagram login',async ({page})=> {
     await page.goto('https://www.instagram.com/accounts/login/#');
-    const loginButton = page.locator('text = Log in')
+    const loginButton = page.locator('button[type="submit"]');
     await expect(loginButton).toBeVisible();
 } 
 );
