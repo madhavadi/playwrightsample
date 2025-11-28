36dc4cd908d8f71cff76953a980b88e165c37623
madhavadi <madhav221095@gmail.com>
2025-11-28 16:19:22 +0530

    Functionality tests

---
+++ tests/mobile/validations.spec.js
@@ -0,0 +1,15 @@
+import {test,expect} from '@playwright/test';
+
+test('Saucedemo validations',async ({page})=>{
+    await page.goto('https://www.saucedemo.com');
+    await page.fill('#user-name','standard_user');
+    await page.fill('#password','secret_sauce');
+    await page.click('#login-button');
+
+    const title = page.locator('.title');
+    const backpack = page.getByText('Sauce Labs Backpack');
+
+    await expect(backpack).toBeTruthy();
+    await expect(title).toContainText('Products');
+    await page.screenshot({path : 'screenshot.png'});
+})
